ece79e6b268d18ee9376d2cf50a7e010a62d59df
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-20 17:12:12 -0300

    implements spring annotations

--- src/main/java/com/gproject/glog/api/controller/ClientController.java
+++ src/main/java/com/gproject/glog/api/controller/ClientController.java
@@ -1,7 +1,12 @@
 package com.gproject.glog.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClientController {
 	
+	@GetMapping(value = "/clients")
 	public String list() {
 		return "String test";
 	}
