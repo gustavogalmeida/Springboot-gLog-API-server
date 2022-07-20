a348d83fd1889ee6b03fbe395943668ce403e6fc
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-20 17:50:16 -0300

    First entity with return ok

--- src/main/java/com/gproject/glog/api/controller/ClientController.java
+++ src/main/java/com/gproject/glog/api/controller/ClientController.java
@@ -1,14 +1,26 @@
 package com.gproject.glog.api.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gproject.glog.domain.model.Client;
 
 @RestController
 public class ClientController {
 	
 	@GetMapping(value = "/clients")
-	public String list() {
-		return "String test";
+	public ResponseEntity<Client> list() {
+		Client client1 = new Client();
+		client1.setId(1L);
+		client1.setEmail("gustavogalvesalmeida@gmail.com");
+		client1.setNome("Gustavo Almeida");
+		client1.setTelefone("3499999999");
+		
+		return ResponseEntity.status(HttpStatus.OK).body(client1);
 	}
 	
 }
--- src/main/java/com/gproject/glog/domain/model/Client.java
+++ src/main/java/com/gproject/glog/domain/model/Client.java
@@ -1,5 +1,10 @@
 package com.gproject.glog.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Client {
 	
 	private Long id;
