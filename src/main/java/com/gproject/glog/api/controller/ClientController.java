d974a66f6e3d1bc73080ca6d271f8b315526b304
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-21 09:11:56 -0300

    lines not used removed

--- src/main/java/com/gproject/glog/api/controller/ClientController.java
+++ src/main/java/com/gproject/glog/api/controller/ClientController.java
@@ -3,8 +3,6 @@ package com.gproject.glog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
