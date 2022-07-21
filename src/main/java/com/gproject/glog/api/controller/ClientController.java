a2bfc733494cf99a869017a21995754972efaa6e
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-21 09:07:21 -0300

    implements new object client for test

--- src/main/java/com/gproject/glog/api/controller/ClientController.java
+++ src/main/java/com/gproject/glog/api/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.gproject.glog.api.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -13,14 +14,20 @@ import com.gproject.glog.domain.model.Client;
 public class ClientController {
 	
 	@GetMapping(value = "/clients")
-	public ResponseEntity<Client> list() {
-		Client client1 = new Client();
+	public List<Client> list() {
+		var client1 = new Client();
 		client1.setId(1L);
 		client1.setEmail("gustavogalvesalmeida@gmail.com");
 		client1.setName("Gustavo Almeida");
-		client1.setPhoneNumber("3499999999");
+		client1.setPhoneNumber("34 99999 9999");
 		
-		return ResponseEntity.status(HttpStatus.OK).body(client1);
+		var client2 = new Client();
+		client2.setId(2L);
+		client2.setName("Pedro");
+		client2.setEmail("pedro@gmail.com");
+		client2.setPhoneNumber("34 98888 8888");
+		
+		return Arrays.asList(client1, client2);
 	}
 	
 }
