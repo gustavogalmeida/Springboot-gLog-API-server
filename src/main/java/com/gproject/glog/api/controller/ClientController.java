5b055468a26b0c7ef11efe5d16491e4c2cf60954
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-21 17:19:36 -0300

    Controller using JPA for consult

--- src/main/java/com/gproject/glog/api/controller/ClientController.java
+++ src/main/java/com/gproject/glog/api/controller/ClientController.java
@@ -3,6 +3,9 @@ package com.gproject.glog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +14,12 @@ import com.gproject.glog.domain.model.Client;
 @RestController
 public class ClientController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping(value = "/clients")
 	public List<Client> list() {
-		var client1 = new Client();
-		client1.setId(1L);
-		client1.setEmail("gustavogalvesalmeida@gmail.com");
-		client1.setName("Gustavo Almeida");
-		client1.setPhoneNumber("34 99999 9999");
-		
-		var client2 = new Client();
-		client2.setId(2L);
-		client2.setName("Pedro1");
-		client2.setEmail("pedro@gmail.com");
-		client2.setPhoneNumber("34 98888 8888");
-		
-		return Arrays.asList(client1, client2);
+		return  entityManager.createQuery("from client", Client.class).getResultList();
 	}
 	
 }
