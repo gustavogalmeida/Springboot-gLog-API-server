9699974f0f6884b0044ec3b67f1a1cca53d948bf
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-21 17:20:26 -0300

    Past class for entity

--- src/main/java/com/gproject/glog/domain/model/Client.java
+++ src/main/java/com/gproject/glog/domain/model/Client.java
@@ -1,15 +1,32 @@
 package com.gproject.glog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "client")
 public class Client {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
+	
 	private String email;
-	private String phoneNumber;
+	
+	@Column(name = "phonenumber")
+	private String phone;
 		
 }
