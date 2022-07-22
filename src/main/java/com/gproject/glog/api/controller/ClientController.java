a85e0f0cf3d39e54972f3feaf79edfa04c17d972
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-22 17:58:21 -0300

    finished POST but PUT is pending

--- src/main/java/com/gproject/glog/api/controller/ClientController.java
+++ src/main/java/com/gproject/glog/api/controller/ClientController.java
@@ -1,35 +1,57 @@
 package com.gproject.glog.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gproject.glog.domain.model.Client;
 import com.gproject.glog.domain.repository.ClientRepository;
 
+@RequestMapping("/clients")
 @RestController
 public class ClientController {
 	
 	@Autowired
 	private ClientRepository clientRepository;
 	
-	@GetMapping("/clients")
+	@GetMapping
 	public List<Client> list() {
 		return clientRepository.findAll();
 	}
 	
-	@GetMapping("/clients/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Client> find(@PathVariable("id") Long id) {
 		
-		Optional<Client> client = clientRepository.findById(id);
+		return clientRepository.findById(id).map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 		
-		return ResponseEntity.status(HttpStatus.OK).body(client.orElse(null));
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client create(@RequestBody Client client) {
+		
+		return clientRepository.save(client);
+	}
+	
+	@PutMapping("/{id}")
+	public ReponseEntity<Client> update(@RequestBody Client client, @PathVariable("id") Long id) {
+		if (!clientRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client = clientRepository.save(client);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(client);
+	}
 }
