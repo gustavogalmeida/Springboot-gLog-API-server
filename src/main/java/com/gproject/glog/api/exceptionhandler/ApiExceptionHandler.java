972627d4a7242406dd2111704b5b451ed51da3b8
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-25 15:02:26 -0300

    improved validations

--- src/main/java/com/gproject/glog/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/gproject/glog/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.gproject.glog.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -13,15 +20,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
-		ExceptionObject error = new ExceptionObject();
+		List<ExceptionObject.Field> fields = new ArrayList<>();
 		
+		for (ObjectError er : ex.getBindingResult().getAllErrors()) {
+			String field = ((FieldError) er).getField();
+			String message = messageSource.getMessage(er, LocaleContextHolder.getLocale());
+			
+			fields.add(new ExceptionObject.Field(field, message));
+		}
+		
+		ExceptionObject error = new ExceptionObject();
 		error.setTitle("One or more fields is invalid.");
 		error.setDateTime(LocalDateTime.now());
 		error.setStatus(status.value());
+		
+		error.setFields(fields);
 		
 		return handleExceptionInternal(ex, error, headers, status, request);
 	}
--- src/main/java/com/gproject/glog/api/exceptionhandler/ExceptionObject.java
+++ src/main/java/com/gproject/glog/api/exceptionhandler/ExceptionObject.java
@@ -1,14 +1,25 @@
 package com.gproject.glog.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ExceptionObject {
+	
 	private String title;
 	private LocalDateTime dateTime;
 	private Integer status;
+	private List<Field> fields;
+	
+	@Getter
+	@AllArgsConstructor
+	public static class Field{
+		private String name;
+		private String description;
+	}
 }
