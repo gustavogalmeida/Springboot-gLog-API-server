b08bcaa2f1da260f07a0e10b8595eef49de6cdae
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-25 09:28:57 -0300

    Adjusted put method and implemented DeleteMapping

--- src/main/java/com/gproject/glog/api/controller/ClientController.java
+++ src/main/java/com/gproject/glog/api/controller/ClientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,13 +46,24 @@ public class ClientController {
 	}
 	
 	@PutMapping("/{id}")
-	public ReponseEntity<Client> update(@RequestBody Client client, @PathVariable("id") Long id) {
+	public ResponseEntity<Client> update(@RequestBody Client client, @PathVariable("id") Long id) {
 		if (!clientRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
 		}
 		
+		client.setId(id);
+		
 		client = clientRepository.save(client);
 		
-		return ResponseEntity.status(HttpStatus.OK).body(client);
+		return ResponseEntity.ok(client);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Client> delete (@PathVariable Long id){
+		if (!clientRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		clientRepository.deleteById(id);		
+		return ResponseEntity.noContent().build();
 	}
 }
